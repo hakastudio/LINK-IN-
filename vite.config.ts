20998dd6d4474727af97586cccde187a89672f70
Heri Kurniawan <kanghery851@gmail.com>
2026-02-12 22:25:13 +0700

    feat: Update build and TS config for Vite
    - Adjust Vite configuration to enable server opening on startup.
    - Enable `allowJs` and `esModuleInterop` in tsconfig.json for better compatibility.
    - Update `jsx` compiler option to `react-jsx` and simplify `include` path.
    - Update importmap order in index.html for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    sourcemap: false
   },
   server: {
     port: 3000,
+    open: true
   }
 });
