626d86fec818575f039f5ee281bad9ed2ad74bea
Heri Kurniawan <kanghery851@gmail.com>
2026-02-14 22:26:03 +0700

    feat: Add new invoice app link
    Adds a new link to the "APLIKSI INVOICE" on the constants file, expanding the available resources.

--- constants.tsx
+++ constants.tsx
@@ -28,7 +28,8 @@ export const MOCK_DATA: BioData = {
     { id: 'l1', title: 'INSTAGRAM', url: 'https://www.instagram.com/heriikurniawann', isPriority: true },
     { id: 'l2', title: 'DEMO APP KASIR', url: 'https://kasirku.hakastudio.online/' },
     { id: 'l3', title: 'DEMO APP TIKET EVENT', url: 'http://eventkuy.hakastudio.online/' },
-    { id: 'l4', title: 'KERJASAMA', url: 'https://bit.ly/4qx42z2' },
+    { id: 'l4', title: 'APLIKSI INVOICE', url: 'https://flowkas.hakastudio.online/' },
+    { id: 'l5', title: 'KERJASAMA', url: 'https://bit.ly/4qx42z2' },
   ],
   featured: [
     {
